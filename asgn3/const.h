d22fcdc4d316b57031f41999295a610724923bc9
Xiang Yu <bupt.aswmtjdsj@gmail.com>
2014-11-18 02:50:19 -0500

    time server and odr server; constants

---
+++ asgn3/const.h
@@ -0,0 +1,18 @@
+#ifndef CONST_H
+#define CONST_H
+
+#include <stdio.h>
+#include <sys/un.h>
+#include <netdb.h>
+#include <ctype.h>
+
+#include "utils.h"
+#include "log.h"
+#include "msg_api.h"
+#include "mainloop.h"
+
+#define ODR_SUN_PATH "ODR_path_xiangyu"
+#define TIM_SERV_SUN_PATH "time_path_xiangyu"
+#define TIM_SERV_PORT 19326
+
+#endif
--- asgn3/odr_server.c
+++ asgn3/odr_server.c
@@ -5,16 +5,5 @@ int main(int argc, const char **argv) {
 	log_server_init(argc, argv);
 	log_info("Test log\n");
 
-    int sock_un_fd;
-    struct sockaddr_un serv_addr, serv_addr_info;
-    char local_host_name[32], req_host_name[32];
-
-    log_info("Server is going to create UNIX Domain socket!\n");
-    // get local host
-    if(gethostname(local_host_name, sizeof(local_host_name)) < 0) {
-        my_err_quit("gethostname error");
-    }
-    log_info("Current node: %s\n", local_host_name);
-
 	return 0;
 }
