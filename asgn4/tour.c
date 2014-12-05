8992eb5d7a0d4c86ea203cbdef248f852cbd9f04
Xiang Yu <bupt.aswmtjdsj@gmail.com>
2014-12-05 00:34:34 -0500

    fix bug

--- asgn4/tour.c
+++ asgn4/tour.c
@@ -11,7 +11,7 @@ void show_tour_list() {
 	struct tour_list_entry * temp_entry = tour_list;
 	int cnt = 0;
 	log_debug("tour_list>\n");
-	log_debug("#id host_name ip_addr\n");
+	log_debug("\t#id\thost_name\tip_addr\n");
 	while(temp_entry != NULL) {
 		log_debug("\t%d\t%s\t%s\n", cnt, temp_entry->host_name, inet_ntoa((temp_entry->ip_addr).sin_addr));
 		cnt++;
@@ -22,36 +22,54 @@ void show_tour_list() {
 int main(int argc, const char **argv) {
 
 	tour_list = NULL;
+	struct tour_list_entry ** p_tail = &tour_list;
 	int code_flag = EXIT_SUCCESS;
+
+	char local_name[HOST_NAME_MAX_LEN], tmp_str[HOST_NAME_MAX_LEN];
+    if(gethostname(tmp_str, sizeof(tmp_str)) < 0) {
+        my_err_quit("gethostname error");
+    }
+    strcpy(local_name, tmp_str);
 
 	if(argc < 2) {
 
-		log_err("No node names detected! Command error!\n");
-		log_warn("Usage: %s <vm_i> ... <vm_j>\n", argv[0]);
-		code_flag = EXIT_FAILURE;
-		goto ALL_DONE;
+		log_info("No names detected! "
+				"Current node <%s> is not the source node!\n", local_name);
 
 	} else {
 		// parse vm node seq
+		log_info("Name sequence detected! "
+				"Current node <%s> is the source node!\n", local_name);
 		log_debug("Parsing node name sequence ...\n");
 		int i;
 		struct hostent * i_host = NULL;
-		for(i = 1; i < argc; i++) {
+		// should add source node itself into tour list
+		for(i = 1; i <= argc; i++) {
+			// check same node should not appear consequently
+			if(i > 1 && i < argc && strcmp(argv[i], argv[i-1]) == 0) {
+				log_err("Same node \"%s\" should not appear consecutively "
+						"in the node sequence!\n", argv[i]);
+				code_flag = EXIT_FAILURE;
+				goto ALL_DONE;
+			}
+
 			// get dest host by name
-			if((i_host = gethostbyname(argv[i])) == NULL) {
+			if((i_host = gethostbyname((i==argc)?local_name:argv[i])) == NULL) {
 				switch(h_errno) {
 					case HOST_NOT_FOUND:
-						log_err("host %s not found!\n", argv[i]);
+						log_err("host %s not found!\n", (i==argc)?local_name:argv[i]);
 						break;
 					case NO_ADDRESS:
 						// case NO_DATA:
-						log_err("host %s is valid, but does not have an IP address!\n", argv[i]);
+						log_err("host %s is valid, but does not have an IP address!\n",
+								(i==argc)?local_name:argv[i]);
 						break;
 					case NO_RECOVERY:
 						log_err("A nonrecoverable name server error occurred.!\n");
 						break;
 					case TRY_AGAIN:
-						log_err("A temporary error occurred on an authoritative name server. Try again later.\n");
+						log_err("A temporary error occurred on "
+								"an authoritative name server. Try again later.\n");
 						break;
 				}
 
@@ -60,14 +78,22 @@ int main(int argc, const char **argv) {
 				goto ALL_DONE;
 			}
 
-			struct tour_list_entry * prev = tour_list;
-			tour_list = (struct tour_list_entry *) malloc(sizeof(struct tour_list_entry));
-			(tour_list->ip_addr).sin_addr = *((struct in_addr *)(i_host->h_addr_list[0]));
-			strcpy(tour_list->host_name, argv[i]);
-			tour_list->next = prev;
-
-			show_tour_list();
+			if(i != argc) {
+				struct tour_list_entry * cur_entry = (struct tour_list_entry *) malloc(sizeof(struct tour_list_entry));
+				(cur_entry->ip_addr).sin_addr = *((struct in_addr *)(i_host->h_addr_list[0]));
+				strcpy(cur_entry->host_name, argv[i]);
+				cur_entry->next = NULL;
+				*p_tail = cur_entry;
+				p_tail = &((*p_tail)->next);
+			} else { // add source node into list
+				struct tour_list_entry * prev = tour_list;
+				tour_list = (struct tour_list_entry *) malloc(sizeof(struct tour_list_entry));
+				(tour_list->ip_addr).sin_addr = *((struct in_addr *)(i_host->h_addr_list[0]));
+				strcpy(tour_list->host_name, local_name);
+				tour_list->next = prev;
+			}
 		}
+		show_tour_list();
 	}
 
 	int sock_rt, sock_pg_reply, sock_pg_send, sock_un; // unix sock, need think
